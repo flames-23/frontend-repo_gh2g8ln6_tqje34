9517e259074fd7db5b0e15a8b2beee6649499673
Flames <vibe-coder@flames.com>
2025-11-08 06:05:25 +0000

    Update project prj_gh2g8ln6 - 2025-11-08T06:05:24.591Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from './components/Hero';
-import Narrative from './components/Narrative';
 import Product from './components/Product';
 import About from './components/About';
 import Footer from './components/Footer';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="min-h-screen w-full bg-slate-950 text-white">
       <Hero />
-      <Narrative />
       <Product />
       <About />
       <Footer />
